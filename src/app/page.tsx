1eade0c83337549535d3a365586feed70284d3d2
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-02-29 13:45:22 +1100

    figma designs and added clouds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,26 @@ const hello = 'hi'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[#225E92]">
       doggie
-      <Image src="/assets/images/wave.svg" alt="buildings" width="1280" height="500"/>
+      <div className="flex relative w-full h-screen">
+        {/* lightbulb idea, put the crane in */}
+        {/* cut up some svgs for the bg and figma work shopped it */}
+        {/* <Image className="absolute bottom-[60px]" src="/assets/images/wave.svg" alt="buildings" width="1280" height="300" /> */}
+        <div>
+        </div>
+
+        <div>
+        </div>
+
+
+
+        <Image className="absolute bottom-0 w-full z-10" src="/assets/images/cloud1.webp" alt="buildings" width="1280" height="300" style={{ objectFit: "cover" }} />
+        <Image className="absolute w-full bottom-0" src="/assets/images/cloud3.png" alt="buildings" width="1280" height="300" style={{ objectFit: "cover" }} />
+      </div>
+
+
+
     </main>
   );
 }
