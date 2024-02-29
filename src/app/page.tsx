966fcd6a07124c507a451afae5accdb7faca32e4
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-02-29 18:08:01 +1100

    added and chopped up imgs in figma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,18 @@ const hello = 'hi'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-[#225E92]">
-      doggie
-      <div className="flex relative w-full h-screen">
-        {/* lightbulb idea, put the crane in */}
-        {/* cut up some svgs for the bg and figma work shopped it */}
-        {/* <Image className="absolute bottom-[60px]" src="/assets/images/wave.svg" alt="buildings" width="1280" height="300" /> */}
-        <div>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between bg-[#225E92]">
+      <div className="relative w-full h-screen">
+        <div className="relative h-full w-full max-w-screen-xl">
+
         </div>
-
-        <div>
-        </div>
+        <img className="w-full absolute bottom-0" src="/assets/images/coverCloud.png" />
 
 
-
-        <Image className="absolute bottom-0 w-full z-10" src="/assets/images/cloud1.webp" alt="buildings" width="1280" height="300" style={{ objectFit: "cover" }} />
-        <Image className="absolute w-full bottom-0" src="/assets/images/cloud3.png" alt="buildings" width="1280" height="300" style={{ objectFit: "cover" }} />
+        <img className="w-full absolute bottom-0" src="/assets/images/longCloud.png" />
       </div>
+
+
 
 
 
