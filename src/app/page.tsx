2f323209294213508873bda8777ab34fb57a3ec6
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-03-01 13:35:51 +1100

    final cloud placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,21 @@ const hello = 'hi'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between bg-[#225E92]">
-      <div className="relative w-full h-screen">
-        <div className="relative h-full w-full max-w-screen-xl">
+    <main className="flex min-h-screen w-full flex-col items-center justify-between bg-[#225E92] overflow-auto">
+
+      <div className="relative w-full h-screen overflow-hidden flex justify-center">
+        <div className="relative max-w-screen-xl h-screen w-full">
+
+          <div className="absolute w-[1280px] bottom-0 overflow-visible">
+            <img className="absolute bottom-0 2xl:-bottom-[8%] z-10" src="/assets/images/coverCloud.png" />
+            <img className="z-20" src="/assets/images/longCloud.png" />
+          </div>
+
 
         </div>
-        <img className="w-full absolute bottom-0" src="/assets/images/coverCloud.png" />
-
-
-        <img className="w-full absolute bottom-0" src="/assets/images/longCloud.png" />
       </div>
+
+      below
 
 
 
